dd5dd5525b57ed6592dcf088b12a1e24b29e2df5
destiaeka <163701104+destiaeka@users.noreply.github.com>
2025-11-21 09:52:06 +0700

    Update monitoring-report.sh

--- monitoring-report.sh
+++ monitoring-report.sh
@@ -1,54 +1,45 @@
 #!/bin/bash
 
-# CONFIG
-CHAT_ID="8355773223"
-BOT_TOKEN="8524070723:AAEdHd5sDikSBFIpsHsZOtI5J_q5NNdX28A"
+# glob_config
+DB_FILE="/root/monitoring/server_monitoring.db"
+SQLITE="/usr/bin/sqlite3"
 
-# HOST INFO
-# - Ambil Data Server
+# create database
+$SQLITE $DB_FILE <<EOF
+CREATE TABLE IF NOT EXISTS server_monitoring (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  host TEXT,
+  time TEXT,
+  uptime TEXT,
+  cpu INTEGER,
+  ram_used INTEGER,
+  ram_total INTEGER,
+  disk_used TEXT,
+  disk_total TEXT,
+  fs_used TEXT,
+  fs_total TEXT,
+  net_rx INTEGER,
+  net_tx INTEGER
+);
+EOF
+
 HOST=$(hostname)
 TIME=$(date '+%d-%m-%Y %H:%M:%S')
 UPTIME=$(uptime -p)
 
-# CPU
-# - menggunakan perintah top, ambil baris CPU, ambil penggunakan CPU. Dibulatkan ke angka bulat
 CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}' | awk '{printf "%.0f", $1}')
-
-# RAM (used/total)
-# - menampilkan penggunakan ram dengan perintah free -m. $3 --> ram terpakai $2 --> ram total.
 RAM_USED=$(free -m | awk '/Mem/ {printf "%d", $3}')
 RAM_TOTAL=$(free -m | awk '/Mem/ {printf "%d", $2}')
-RAM="${RAM_USED}Mi/${RAM_TOTAL}Mi"
-
-# DISK root / (used/total)
-# - menampilkan penggunakan disk partisi root /. NR --> ambil baris ke2 (data partisi)
 DISK_USED=$(df -h / | awk 'NR==2 {print $3}')
 DISK_TOTAL=$(df -h / | awk 'NR==2 {print $2}')
-DISK="${DISK_USED}/${DISK_TOTAL}"
-
-# FILE SYSTEM root / (used/total)
-FS_USED=$(df -h / | awk 'NR==2 {print $3}')
-FS_TOTAL=$(df -h / | awk 'NR==2 {print $2}')
-FS="/:${FS_USED}/${FS_TOTAL}"
-
-# NETWORK (main interface)
-# - NET_RX: jumlah byte diterima
-# - NET_TX: jumlah byte dikirim
-IFACE="enp0s3"
+FS_USED=$DISK_USED
+FS_TOTAL=$DISK_TOTAL
+IFACE="eth0"
 NET_RX=$(cat /sys/class/net/$IFACE/statistics/rx_bytes)
 NET_TX=$(cat /sys/class/net/$IFACE/statistics/tx_bytes)
-NET="Network ($IFACE): RX=${NET_RX}B / TX=${NET_TX}B"
 
-# FORMAT MESSAGE
-MESSAGE="🖥 Server: $HOST
-⏱ Time: $TIME | Uptime: $UPTIME
-⚡ CPU: ${CPU}%
-💾 RAM: $RAM
-💽 DISK: $DISK
-🌐 NET: $NET"
-
-# SEND TO TELEGRAM
-curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
-     -d chat_id="$CHAT_ID" \
-     -d parse_mode="Markdown" \
-     -d text="$MESSAGE"
+# insert data
+$SQLITE $DB_FILE <<EOF
+INSERT INTO server_monitoring (host, time, uptime, cpu, ram_used, ram_total, disk_used, disk_total, fs_used, fs_total, net_rx, net_tx)
+VALUES ('$HOST', '$TIME', '$UPTIME', $CPU, $RAM_USED, $RAM_TOTAL, '$DISK_USED', '$DISK_TOTAL', '$FS_USED', '$FS_TOTAL', $NET_RX, $NET_TX);
+EOF
